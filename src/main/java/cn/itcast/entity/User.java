0a585e3337ce95cc44b09aad13e61ea75868c835
xushifeng <xushifeng@itcast.cn>
2020-08-02 17:15:32 +0800

    初始项目提交

---
+++ src/main/java/cn/itcast/entity/User.java
@@ -0,0 +1,9 @@
+package cn.itcast.entity;
+
+/**
+ * 用户实体
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
---
+++ src/test/java/cn/itcast/Demo.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+import cn.itcast.entity.User;
+
+/**
+ *
+ */
+public class Demo {
+    public static void main(String[] args) {
+        new User();
+    }
+}
